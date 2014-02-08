82a9403362730f47fb5497a9c0de4f6427c4da9f
Thomas Dupouy <moz@gmx.fr>
2014-02-08 16:53:39 +0100

    Forgot to add plugins.py to Git

---
+++ opendmp/tools/plugins.py
@@ -0,0 +1,30 @@
+''' from http://onehourhacks.blogspot.ie/2013/04/a-simple-python-plugin-framework.html 
+The general idea is that you give the function a path then it iterates through the path, 
+finding python files, loading them, and then searching them for any classes that are 
+subclasses of the class you want. When it gets done, it returns them.
+'''
+from tools.log import Log; stdlog = Log.stdlog
+import inspect, os, sys, importlib
+
+class Plugins:
+    pass
+
+def load_plugins(dirname):
+    '''Loads a set of plugins at the given path.'''
+    plugins = []
+    plugin_dir = os.path.normpath(os.path.join(
+                        os.path.abspath(os.path.join(
+                                os.path.dirname(__file__),'..',dirname))))
+
+    for f in os.listdir(plugin_dir):
+        if f.endswith(".py"): 
+            name = f[:-3]
+            try:
+                mod = importlib.import_module('bu.' + name)
+                for (mod_type, mod_piece) in inspect.getmembers(mod):
+                    if issubclass(mod_piece, Plugins) and not mod_piece == Plugins:
+                        plugins.append(mod_piece)
+            except (TypeError, ImportError):
+                pass 
+
+    return plugins
